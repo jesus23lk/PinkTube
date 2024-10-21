f30893d79af52cc18279b23f4a577ff07b55c6e7
Jesus Garcia <jesus23lk@gmail.com>
2024-10-21 15:23:20 -0700

    first build

---
+++ code.js
@@ -0,0 +1,7 @@
+document.querySelector('ytd-app').style.backgroundColor = 'pink';
+document.querySelectorAll("div").forEach(div => {
+  div.style.backgroundColor = 'pink';
+});
+
+
+document.querySelector(".YtSearchboxComponentInputBox").style.backgroundColor = "white";
